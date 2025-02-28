39a2389b5813d237e59b4cfe7823da25063fa669
ranselorm <ranselorm@outlook.com>
2025-02-28 21:01:15 +0000

    Make navlinks active

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { Icon } from "@iconify/react";
 
 type SidebarProps = {
@@ -9,9 +9,9 @@ type SidebarProps = {
 const Sidebar = ({ isOpen, toggle }: SidebarProps) => {
   return (
     <aside
-      className={`h-screen fixed top-0 left-0 bg-white text-black transition-all border border-grey ${
+      className={`h-screen fixed top-0 left-0 bg-white text-black transition-all border border-grey p-4 ${
         isOpen ? "w-64" : "w-0"
-      } lg:w-64 flex flex-col p-4`}
+      } lg:w-64 flex flex-col`}
     >
       <div className="lg:hidden">
         <button onClick={toggle} className="text-white p-2">
@@ -22,16 +22,21 @@ const Sidebar = ({ isOpen, toggle }: SidebarProps) => {
         <img src="/berth.png" alt="" className="w-full" />
       </div>
       <nav className="flex flex-col space-y-4 py-4 px-2">
-        <Link
+        <NavLink
           to="/"
-          className="flex items-center space-x-2 p-2 hover:bg-teal-500 hover:text-white transition-all duration-150 rounded-md"
+          //   className="flex items-center space-x-2 p-2 hover:bg-teal-500 hover:text-white transition-all duration-150 rounded-md"
+          className={({ isActive }) =>
+            `${
+              isActive ? "bg-red-400" : "bg-yellow-400"
+            } flex items-center space-x-2 p-2 hover:bg-teal-500 hover:text-white transition-all duration-150 rounded-md`
+          }
         >
           <Icon
             icon="material-symbols-light:dashboard-outline"
             className="text-xl"
           />
           <span className="hidden lg:block">Dashboard</span>
-        </Link>
+        </NavLink>
         <Link
           to="/settings"
           className="flex items-center space-x-2 p-2 hover:bg-gray-700"
