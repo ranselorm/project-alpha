ddd2f7d8ab501f16ec6067c6cace9938a4d0572e
ranselorm <ranselorm@outlook.com>
2025-02-28 17:01:43 +0000

    Modify navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify/react";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 type NavbarProps = {
   toggleSidebar: () => void;
@@ -15,19 +16,23 @@ const Navbar = ({ toggleSidebar }: NavbarProps) => {
       >
         <Icon icon="hugeicons:menu-03" className="size-6" />
       </button>
-      <div className="flex justify-between w-full items-center">
-        <div className="flex items-start flex-col">
+      <div className="flex justify-between w-full items-center gap-x-20">
+        {/* <div className="flex items-start flex-col">
           <span className="text-sm">Welcome,</span>
           <p className="font-semibold">Randy Selorm</p>
-        </div>
-        <Button>Helo</Button>
-        <p>Search</p>
-        <div className="flex gap-x-3">
-          <div className="bg-white text-black p-2 flex items-center justify-center rounded-full">
-            <Icon icon="stash:question-light" className="size-4" />
+        </div> */}
+        <h3 className="font-bold text-2xl">Home</h3>
+        <div className="flex gap-x-6">
+          <div className="w-[400px]">
+            <Input className="bg-white border-none outline-none focus:ring-none" />
           </div>
-          <div className="bg-white text-black p-2 flex items-center justify-center rounded-full">
-            <Icon icon="iconamoon:notification-thin" className="size-4" />
+          <div className="flex gap-x-3">
+            <div className="bg-white text-black p-2 flex items-center justify-center rounded-full cursor-pointer">
+              <Icon icon="stash:question-light" className="size-4" />
+            </div>
+            <div className="bg-white text-black p-2 flex items-center justify-center rounded-full cursor-pointer">
+              <Icon icon="iconamoon:notification-thin" className="size-4" />
+            </div>
           </div>
         </div>
       </div>
