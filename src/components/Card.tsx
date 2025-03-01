f83364eafd00b2b7af1329ddcaf810d98bc1def8
ranselorm <ranselorm@outlook.com>
2025-03-01 01:02:21 +0000

    Finish card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,20 +4,22 @@ interface CardProps {
   title: string;
   value: number;
   bgColor?: string;
+  icon?: string;
 }
 
-const Card = () => {
+const Card: React.FC<CardProps> = ({ title, value, bgColor, icon }) => {
   return (
-    <div className="bg-green-100 p-4 flex items-center gap-x-5 rounded-md border border-gray-200">
+    <div
+      className={`${
+        bgColor ? `bg-teal-50` : "bg-white"
+      } p-4 flex items-center gap-x-5 rounded-md border border-gray-200`}
+    >
       <div className="bg-grey text-black p-3 flex items-center justify-center rounded-full cursor-pointer w-12 h-12">
-        <Icon icon="lineicons:dollar" className="size-6 text-teal-600" />
+        <Icon icon={`${icon}`} className="size-6 text-teal-600" />
       </div>
       <div>
-        <span className="text-gray-600 text-sm">Earnings</span>
-        <p className="flex items-center text-2xl">
-          {/* <Icon icon="fa6-solid:cedi-sign" /> */}
-          GHS4500
-        </p>
+        <span className="text-gray-600 text-sm">{title}</span>
+        <p className="flex items-center text-2xl">{value}</p>
       </div>
     </div>
   );
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,10 +6,30 @@ const Dashboard = () => {
       <div className="container mx-container p-4">
         <section>
           <div className="grid grid-cols-1 md:grid-cols-4 gap-x-6">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <Card
+              title="Earnings"
+              value={4700}
+              bgColor="red-100"
+              icon="lineicons:dollar"
+            />
+            <Card
+              title="Users"
+              value={1200}
+              bgColor="blue-100"
+              icon="mdi:users-outline"
+            />
+            <Card
+              title="Reports"
+              value={22}
+              bgColor="blue-100"
+              icon="mage:chart-up"
+            />
+            <Card
+              title="Transactions"
+              value={46}
+              bgColor="blue-100"
+              icon="grommet-icons:transaction"
+            />
           </div>
         </section>
       </div>
