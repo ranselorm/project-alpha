7761171200ff3480d3bd079c4580291d7c3cb2dd
ranselorm <ranselorm@outlook.com>
2025-03-08 20:41:28 +0000

    Cleanup

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -30,32 +30,15 @@ const LineChart = ({ data, labels }: LineChartProps) => {
       {
         label: "Male",
         data,
-        borderColor: "rgb(59, 130, 246)", // Tailwind Blue-500
-        backgroundColor: "rgba(59, 130, 246, 0.2)", // Light fill color
+        borderColor: "rgb(59, 130, 246)",
+        backgroundColor: "rgba(59, 130, 246, 0.2)",
         borderWidth: 2,
-        tension: 0.4, // Curve effect
+        tension: 0.4,
       },
     ],
   };
 
-  //   const options = {
-  //     responsive: true,
-  //     maintainAspectRatio: false,
-  //     plugins: {
-  //       legend: { display: true, position: "top" },
-  //       tooltip: { enabled: true },
-  //     },
-  //     scales: {
-  //       x: { grid: { display: false } },
-  //       y: { beginAtZero: true },
-  //     },
-  //   };
-
-  return (
-    // <div className="w-4/6 h-80 p-4 bg-white shadow-md rounded-lg my-12">
-    <Line data={chartData} />
-    // </div>
-  );
+  return <Line data={chartData} />;
 };
 
 export default LineChart;
