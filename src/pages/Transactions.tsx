ce6e825f7d9120cc3c7489eca0ab1e9da5a67128
ranselorm <ranselorm@outlook.com>
2025-03-08 20:37:30 +0000

    Refactor

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -4,7 +4,6 @@ const Transactions = () => {
   return (
     <div className="p-4">
       <div className="container mx-auto">
-        {/* <p className="my-5">All Transactions</p> */}
         <div className="flex text-sm gap-x-7 mb-4">
           <p className="border-b border-main text-main p-3">All</p>
           <p className="p-3">Weekly</p>
