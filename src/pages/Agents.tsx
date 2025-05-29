cb1f8ea657b5ca0d5a30a883a71cae8f105f3277
ranselorm <ranselorm@outlook.com>
2025-05-29 12:57:49 +0000

    Add search

--- src/pages/Agents.tsx
+++ src/pages/Agents.tsx
@@ -1,129 +1,31 @@
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import UserFilter from "@/components/UserFilter";
-import UsersTable from "@/components/UsersTable";
-import { Icon } from "@iconify/react";
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-  DialogTitle,
-  DialogFooter,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { toast } from "sonner";
-import { useInvite } from "@/hooks/useInvite";
-import { useInvitedUsers } from "@/hooks/useInvitedUsers";
-import InvitedUsersTable from "@/components/InvitedUsersTable";
+import { Input } from "antd";
 
 const Agents = () => {
-  const [phone, setPhone] = useState("");
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [activeTab, setActiveTab] = useState("current");
-
-  const inviteMutation = useInvite();
-  // const { data: invitedUsers, isLoading: loadingInvitedUsers } =
-  //   useInvitedUsers();
-
-  const handleInvite = () => {
-    if (!phone) return;
-
-    inviteMutation.mutate(
-      { phone },
-      {
-        onSuccess: () => {
-          toast.success("Invitation sent successfully!", {
-            duration: 3000,
-          });
-          setTimeout(() => setIsDialogOpen(false), 500);
-        },
-        onError: (error) => {
-          console.error("Error sending invite:", error);
-          toast.error("Failed to send invite. Try again.");
-        },
-      }
-    );
-  };
-
   return (
-    // <div className="p-4">
-    //   <div className="container mx-auto">
-    //     <div className="flex justify-between items-center">
-    //       <div className="flex space-x-4 border-b">
-    //         <button
-    //           className={`px-4 py-2 cursor-pointer ${
-    //             activeTab === "current"
-    //               ? "border-b-2 border-main "
-    //               : "text-gray-500"
-    //           }`}
-    //           onClick={() => setActiveTab("current")}
-    //         >
-    //           Current Users
-    //         </button>
-    //         <button
-    //           className={`px-4 py-2 cursor-pointer ${
-    //             activeTab === "invited"
-    //               ? "border-b-2 border-main"
-    //               : "text-gray-500"
-    //           }`}
-    //           onClick={() => setActiveTab("invited")}
-    //         >
-    //           Invited Users
-    //         </button>
-    //       </div>
-    //       <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-    //         <DialogTrigger asChild>
-    //           <Button className="cursor-pointer bg-main text-white flex items-center gap-x-2 rounded-md hover:bg-teal-700 transition">
-    //             <Icon icon="material-symbols:add-rounded" />
-    //             Invite user
-    //           </Button>
-    //         </DialogTrigger>
-    //         <DialogContent className="p-6 max-w-sm">
-    //           <DialogTitle>Invite a User</DialogTitle>
-    //           <p className="text-sm text-gray-500">
-    //             Enter the user's phone number below:
-    //           </p>
-
-    //           <Input
-    //             type="tel"
-    //             placeholder="+233 55 123 4567"
-    //             value={phone}
-    //             onChange={(e) => setPhone(e.target.value)}
-    //             className="mt-3 focus-visible:ring-0"
-    //           />
-
-    //           <DialogFooter>
-    //             <Button
-    //               onClick={handleInvite}
-    //               disabled={!phone || inviteMutation.isPending}
-    //               className="bg-main cursor-pointer hover:bg-teal-700 text-white mt-4"
-    //             >
-    //               {inviteMutation.isPending
-    //                 ? "Please wait..."
-    //                 : "Send Invitation"}
-    //             </Button>
-    //           </DialogFooter>
-    //         </DialogContent>
-    //       </Dialog>
-    //     </div>
-
-    //     {activeTab === "current" ? (
-    //       <div className="mt-5">
-    //         <UserFilter />
-    //         <UsersTable />
-    //       </div>
-    //     ) : (
-    //       <div className="mt-5">
-    //         {loadingInvitedUsers ? (
-    //           <p>Loading invited users...</p>
-    //         ) : (
-    //           <InvitedUsersTable invitedUsers={invitedUsers ?? []} />
-    //         )}
-    //       </div>
-    //     )}
-    //   </div>
-    // </div>
-    <div className="p-4">Agents</div>
+    <section className="p-4">
+      <main className="container mx-auto">
+        {/* header */}
+        <div className="text-center">
+          <h1 className="text-2xl font-semibold mb-2"> AI Agents Directory</h1>
+          <p>
+            Your One-Stop Destination to Explore and Learn About Modern AI
+            Agents
+          </p>
+        </div>
+        <div className="mx-auto text-center my-5">
+          <Input
+            placeholder="Search for AI Agents here..."
+            className="bg-gray-100 !px-4 !py-2 border-none focus:ring-2 focus:ring-main !rounded-full !w-[50%]"
+          />
+        </div>
+        <section>
+          <div className="bg-white w-full flex justify-between items-center px-4 py-2 rounded-lg ">
+            <div>Showing(21)</div>
+            <div>Name</div>
+          </div>
+        </section>
+      </main>
+    </section>
   );
 };
 
