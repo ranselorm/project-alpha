db22522f3bd9a35461aa15c5851c834e433323b4
ranselorm <ranselorm@outlook.com>
2025-05-29 15:21:02 +0000

    Add trademark

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="text-xs flex flex-col lg:flex-row justify-center items-center gap-4 py-4">
+      <div className="flex gap-x-4 justify-center">
+        <div className="flex items-center">
+          <p className="text-sm">Powered by</p>
+          <div className="w-24 h-auto">
+            <img src="/berth.png" alt="logo" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
