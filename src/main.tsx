2ddfc84400da25ede33cc9537ffa238af96dd8fd
ranselorm <ranselorm@outlook.com>
2025-06-04 22:49:49 +0000

    Clean up

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import { Toaster } from "sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
-import ScrollToTop from "./components/ScrollToTop.tsx";
 
 const client = new QueryClient();
 
@@ -16,7 +15,6 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={client}>
       <Provider store={store}>
         <Router>
-          <ScrollToTop />
           <App />
           <Toaster richColors />
         </Router>
