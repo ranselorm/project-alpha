8e7c9cf13638bdc1a2c2394bfc6d9e4b2ca754c7
ranselorm <ranselorm@outlook.com>
2025-06-06 17:59:30 +0000

    Add history ui

--- src/pages/TrainAgent.tsx
+++ src/pages/TrainAgent.tsx
@@ -4,6 +4,37 @@ import { Button, Modal } from "antd";
 
 type TabKey = "Train" | "second" | "longtext";
 
+const hsitory = [
+  {
+    id: 1,
+    title: "Company Profile",
+    description: "This is a sample description",
+    type: "PDF",
+    timeAgo: "2 days ago",
+  },
+  {
+    id: 2,
+    title: "Company Profile",
+    description: "This is a sample description",
+    type: "Website",
+    timeAgo: "2 days ago",
+  },
+  {
+    id: 3,
+    title: "Company Profile",
+    description: "This is a sample description",
+    type: "Website",
+    timeAgo: "2 days ago",
+  },
+  {
+    id: 4,
+    title: "Company Profile",
+    description: "This is a sample description",
+    type: "Website",
+    timeAgo: "2 days ago",
+  },
+];
+
 const TrainAgent: React.FC = () => {
   const [activeKey, setActiveKey] = useState<TabKey>("Train");
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -20,12 +51,31 @@ const TrainAgent: React.FC = () => {
     setIsModalOpen(false);
   };
 
-  // Define the content for each tab
   const dataForTabs: Record<TabKey, React.ReactNode> = {
     Train: (
       <div className="flex items-center space-x-4">
-        <div className="bg-white w-[300px] h-[70vh] rounded-md">1</div>
-        <div className="bg-white flex-grow h-[70vh] rounded-md">2</div>
+        <div className="bg-white w-[300px] h-[70vh] rounded-md overflow-y-auto border border-gray-200">
+          <div className="flex flex-col space-y-2 px-4">
+            {hsitory.map((item) => (
+              <div
+                key={item.id}
+                className="border-b border-gray-200 py-2 cursor-pointer hover:bg-gray-50"
+              >
+                <span className="text-[10px] font-semibold uppercase">
+                  {item.type}
+                </span>
+                <h3 className="font-semibold">{item.title}</h3>
+                <p className="text-sm text-gray-500">{item.description}</p>
+                <span className="text-xs text-gray-400 justify-end flex">
+                  {item.timeAgo}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="bg-white flex-grow h-[70vh] rounded-md border border-gray-200">
+          2
+        </div>
       </div>
     ),
     second: (
@@ -64,33 +114,47 @@ const TrainAgent: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto">
-      <div className="flex space-x-8 mb-2 border-b border-gray-300">
-        {Object.keys(tabNames).map((key) => (
-          <div
-            key={key}
-            className={`cursor-pointer py-2 ${
-              activeKey === key
-                ? "border-b-2 border-main font-semibold"
-                : "text-gray-500"
-            }`}
-            onClick={() => setActiveKey(key as TabKey)}
+    <section>
+      <Modal
+        title="Basic Modal"
+        closable={{ "aria-label": "Custom Close Button" }}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+
+      <div className="container mx-auto">
+        <div className="flex space-x-8 mb-2 border-b border-gray-300">
+          {Object.keys(tabNames).map((key) => (
+            <div
+              key={key}
+              className={`cursor-pointer py-2 ${
+                activeKey === key
+                  ? "border-b-2 border-main font-semibold"
+                  : "text-gray-500"
+              }`}
+              onClick={() => setActiveKey(key as TabKey)}
+            >
+              {tabNames[key as TabKey]}
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-end mb-2">
+          <Button
+            className="!bg-main !text-white !border-none"
+            onClick={showModal}
           >
-            {tabNames[key as TabKey]}
-          </div>
-        ))}
+            <Icon icon="material-symbols:add-rounded" className="text-white" />
+            Add
+          </Button>
+        </div>
+        <div>{dataForTabs[activeKey]}</div>
       </div>
-      <div className="flex justify-end mb-2">
-        <Button
-          className="!bg-main !text-white !border-none"
-          onClick={showModal}
-        >
-          <Icon icon="material-symbols:add-rounded" className="text-white" />
-          Add
-        </Button>
-      </div>
-      <div>{dataForTabs[activeKey]}</div>
-    </div>
+    </section>
   );
 };
 
